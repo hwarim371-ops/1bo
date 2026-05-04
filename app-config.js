f4727f77ea18586b447d9a00631925c8d948a321
hwarim371-ops <hwarim371@gmail.com>
2026-05-05 08:11:16 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,7 +1,10 @@
 window.APP_CONFIG = {
   projects: [
-    { name: "제천2조은삼", syncEndpoint: "https://script.google.com/macros/s/AKfycbwVT19EDx_npBnKwp1IJnEYdEAOwHRIHomix5_sopls96-RpUdWGZ4_kh9hpBMXafcaHA/exec", spreadsheetId: "" },
-    { name: "천안", syncEndpoint: "https://script.google.com/macros/s/AKfycbyXmusdnoyY3dY-DlBtmfyh9folJjFRpTruM-GpAlFeq7RLaFcK1hT0RCsTf1ilZ8V3fQ/exec", spreadsheetId: "" },
-    { name: "화순", syncEndpoint: "https://script.google.com/macros/s/AKfycbzJoXX0KLV8eFXe7fp9dpBDA51QIhSJDc1UG6XjbWqdQKkJmWxIE2kWe_h0g4UPgOIw/exec", spreadsheetId: "" }
+    { name: "제천2조은삼", syncEndpoint: 
+"https://script.google.com/macros/s/AKfycbz-4hp6M3NhmqiZzM75iXLzGgW5jV4FnEFQxPFpQ4EM4Q-3vuejgypzLwSImPJsO0q7ZA/exec", spreadsheetId: "" },
+    { name: "천안", syncEndpoint: 
+"https://script.google.com/macros/s/AKfycbw7Xf8lfFtPxOW-TZ0R-RXeW5XkNlaxwiDIGHwPaYIqDOBURK3_2_znUrB7nw5xTfWKng/exec", spreadsheetId: "" },
+    { name: "화순", syncEndpoint: 
+"https://script.google.com/macros/s/AKfycbyeFTrdIH4M_E-EbpP5dfn_fzoWHk7tRuko8tBcDEnMYJkXRXl-oQtqINPFMSs1u74/exec", spreadsheetId: "" }
   ]
 };
