b6c2f55cf59f353a882fd167f1f205e3ffe7dce2
hwarim371-ops <hwarim371@gmail.com>
2026-05-05 01:13:05 +0900

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   projects: [
-    { name: "제천2덕동골", syncEndpoint: "", spreadsheetId: "" },
-    { name: "밀양", syncEndpoint: "", spreadsheetId: "" },
-    { name: "서울", syncEndpoint: "https://script.google.com/macros/s/AKfycbwzRAIj_oFSt-d3JRU7_gJhx-UFv0zTOgE3TlvfHdsmSZ1XXFZldivsZKmUA-GYnlVw8w/exec", spreadsheetId: "" }
+    { name: "제천2조은삼", syncEndpoint: "https://script.google.com/macros/s/AKfycbwJPKbnFOZfApW9FSJJyjT0A3RWPp5DRoHOgTcB3UGvqxoTelGfk82m5A7HYcdexH18iA/exec", spreadsheetId: "" },
+    { name: "천안", syncEndpoint: "https://script.google.com/macros/s/AKfycbwrjs35hFF6CbXFGRJd7Pen4YQ5KyEJzXMlZI5DjrG5vdq6xt9_201g_rNKYoYj8-0VpQ/exec", spreadsheetId: "" },
+    { name: "화순", syncEndpoint: "https://script.google.com/macros/s/AKfycbwzUz2RjDFL6NjBlYkUQGjV4WWQ8_745Yu5a3du5b0zt8NRo7imrmuonD6nj_BgNhFL/exec", spreadsheetId: "" }
   ]
 };
