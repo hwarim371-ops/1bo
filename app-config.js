675c54948122783459f88498267723841e646cbe
hwarim371-ops <hwarim371@gmail.com>
2026-06-12 08:39:37 +0900

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -2,8 +2,8 @@ window.APP_CONFIG = {
   // 비밀번호는 이 파일에 넣지 않습니다. 각 프로젝트 Apps Script의
   // setupProjectPassword 함수로 Script Properties에 설정합니다.
   projects: [
-    { name: "제천2조은삼", syncEndpoint: "https://script.google.com/macros/s/AKfycbwd0Xg0n3VzRbUpUQiAsVax_gxEBp6k28yUO-ym6pJctC6Ttqqg3AhPDWy_xQqS4YyyrQ/exec", spreadsheetId: "" },
-    { name: "천안1", syncEndpoint: "https://script.google.com/macros/s/AKfycbz3TIoUNcffTgyoHjG26Qi5J5PJpVxOjXQY2YDtVHRat5fsDKg5YY_v0CODJ9AqD6n08w/exec", spreadsheetId: "" },
-    { name: "화순", syncEndpoint: "https://script.google.com/macros/s/AKfycbyjs2WgW8FCNI-PALVf7VEls_jWVhuV-3anlRPz-A6C0HwZqVf3s1aBynsf9E7uJbtn/exec", spreadsheetId: "" }
+    { name: "제천2조은삼", syncEndpoint: "https://script.google.com/macros/s/AKfycbz3wCf04MTJ3Rk1FGRgQJhjvT3vHdNxgs_Pnlr_qfg8Jbgg--m3YMXiUkK13XDa44IoBg/exec", spreadsheetId: "" },
+    { name: "천안1", syncEndpoint: "https://script.google.com/macros/s/AKfycbyJBw8CpgutBTDl5Slf8buexrya7vC5PEm9wNUo1u23W09z4ahKLS0YRfhQ0NO_TWHAXw/exec", spreadsheetId: "" },
+    { name: "화순", syncEndpoint: "https://script.google.com/macros/s/AKfycbyCDMm4U02Ifz_N2kcNuAQIjK_nd4ig3cjNeMk8qlAk6ey4luqCCC7aI1gAQxZhNC8/exec", spreadsheetId: "" }
   ]
 };
